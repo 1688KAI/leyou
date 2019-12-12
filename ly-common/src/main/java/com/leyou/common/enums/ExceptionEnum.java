82e32d9dcb88857f361f2da537da5fa740745bd6
sunkai <10020sunkai@qq.com>
2019-12-12 09:46:26 +0800

    leyou good first commit

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -3,15 +3,16 @@ package com.leyou.common.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public enum  ExceptionEnum{
-    PRICE_CANNOT_BE_NULL(400, "价格不能为空"),
-    PRICE_CANNOT_BE_NULL1(400, "价格不能为空"),
-    PRICE_CANNOT_BE_NULL2(400, "价格不能为空"),
-    PRICE_CANNOT_BE_NULL3(400, "价格不能为空");
+    PRICE_CANNOT_BE_NULL(HttpStatus.NOT_FOUND.value(), "价格不能为空"),
+    PRICE_CANNOT_BE_NULL1(HttpStatus.NOT_FOUND.value(), "价格不能为空"),
+    PRICE_CANNOT_BE_NULL2(HttpStatus.NOT_FOUND.value(), "价格不能为空"),
+    PRICE_CANNOT_BE_NULL3(HttpStatus.NOT_FOUND.value(), "价格不能为空");
     private int code;
     private String msg;
 }
--- ly-item/ly-item-interface/src/main/java/com/leyou/item/pojo/Item.java
+++ ly-item/ly-item-interface/src/main/java/com/leyou/item/pojo/Item.java
@@ -1,5 +1,6 @@
 package com.leyou.item.pojo;
 
+import com.sun.istack.internal.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Item implements Serializable {
+
+    @NotNull
     private Double price;
 }
